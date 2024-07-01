f9ece34438025e68f9e8bb427e46ee303c531e7c
zuhdi <rakazhp@gmail.com>
2024-07-01 16:25:39 +0700

    Fixed component structure

--- src/components/pages/Messaging/Messaging.js
+++ src/components/pages/Messaging/Messaging.js
@@ -4,25 +4,29 @@ import {Card, CardBody, Container} from "reactstrap";
 import Palette from "../../../utils/Palette";
 
 export default function Messaging() {
-
     const [form, setForm] = useState({title: '', body: ''})
+
+    const CustomField = ({title, onChange, rows}) => {
+        return (
+            <Form.Group className="mb-3">
+                <Form.Label style={{fontSize: "0.8em"}}>{title}</Form.Label>
+                <Form.Control as={rows && 'textarea'} rows={rows} onChange={(e) => onChange(e.target.value)} type="text" placeholder="Title"/>
+            </Form.Group>
+        )
+    }
+
     return (
         <Container fluid>
-            <Card style={{background: Palette.BACKGROUND_DARK_GRAY, color: "white"}} className="card-stats mb-4 mb-xl-0">
+            <Card style={{background: Palette.BACKGROUND_DARK_GRAY, color: "white"}}
+                  className="card-stats mb-4 mb-xl-0">
                 <CardBody>
                     <Row className={'mb-4'}>
                         <Col className='mb-3' md={12}>
                             <div style={{fontWeight: "bold", fontSize: "1.1em"}}>Top Up</div>
                         </Col>
                     </Row>
-                    <Form.Group className="mb-3">
-                        <Form.Label style={{fontSize: "0.8em"}}>Title</Form.Label>
-                        <Form.Control value={form.title} onChange={(e) => setForm({...form, title: e.target.value})} type="text" placeholder="Title"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label style={{fontSize: "0.8em"}}>Body</Form.Label>
-                        <Form.Control rows={4} as={'textarea'} value={form.body} onChange={(e) => setForm({...form, body: e.target.value})} type="text" placeholder="Title"/>
-                    </Form.Group>
+                    <CustomField title={'Title'} onChange={(e) => setForm({...form, title: e.target.value})}/>
+                    <CustomField title={'Body'} rows={4} as={'textarea'} value={form.body} onChange={(e) => setForm({...form, body: e.target.value})} type="text" placeholder="Body"/>
                 </CardBody>
             </Card>
         </Container>

72d7b2abc7b4d3fc2fc526e98389e2b9e1e1ef3b
Michael Geraldi <work.michaelgeraldi@gmail.com>
2024-07-09 12:09:33 +0700

    improve chart styling in DashboardColumnChartWidget

--- src/components/pages/Dashboard/DashboardColumnChartWidget.js
+++ src/components/pages/Dashboard/DashboardColumnChartWidget.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { Flex } from 'antd';
 import { Bar } from 'react-chartjs-2';
+import Palette from 'utils/Palette';
 import DashboardWidgetContainer from './DashboardWidgetContainer';
 
 export default function DashboardColumnChartWidget(props) {
@@ -13,45 +14,32 @@ export default function DashboardColumnChartWidget(props) {
 			{
 				label: 'My First Dataset',
 				data: [65, 59, 80, 81, 56, 55, 40],
-				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(255, 159, 64, 0.2)',
-					'rgba(255, 205, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(201, 203, 207, 0.2)',
-				],
-				borderColor: [
-					'rgb(255, 99, 132)',
-					'rgb(255, 159, 64)',
-					'rgb(255, 205, 86)',
-					'rgb(75, 192, 192)',
-					'rgb(54, 162, 235)',
-					'rgb(153, 102, 255)',
-					'rgb(201, 203, 207)',
-				],
-				borderWidth: 1,
+				backgroundColor: new Array(7).fill(Palette.BARCODE_ORANGE),
+				borderRadius: 4,
 			},
 		],
 	};
 
+	const chartOptions = {
+		barThickness: 24,
+	};
+
 	useEffect(() => {
 		setChartSize({
-			height: containerRef.current.clientHeight,
-			width: containerRef.current.clientWidth,
-		});
-
-		console.log('CHART SIZE', {
-			height: containerRef.current.clientHeight,
-			width: containerRef.current.clientWidth,
+			height: containerRef.current.clientHeight - 40, // vertical padding = 20 * 2
+			width: containerRef.current.clientWidth - 40, // horizontal padding = 20 * 2
 		});
 	}, [containerRef]);
 
 	return (
 		<DashboardWidgetContainer title={props.title}>
 			<Flex flex={1} ref={containerRef}>
-				<Bar data={chartData} height={chartSize?.height || 0} width={chartSize?.width || 0} />
+				<Bar
+					data={chartData}
+					height={chartSize?.height || 0}
+					width={chartSize?.width || 0}
+					options={chartOptions}
+				/>
 			</Flex>
 		</DashboardWidgetContainer>
 	);

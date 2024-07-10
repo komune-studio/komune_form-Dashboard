ce04820808b5370124e1b1ca908ffa9ab7b0c089
Michael Geraldi <work.michaelgeraldi@gmail.com>
2024-07-10 10:53:27 +0700

    add new table & integrate API for getting rides usages history

--- src/components/pages/Order/OrderList.js
+++ src/components/pages/Order/OrderList.js
@@ -1,144 +1,160 @@
-import { Button, Card, CardBody, Container, Row, Col } from "reactstrap";
-import Palette from "../../../utils/Palette";
-import TopUpTitleBar from "../TopUp/TopUpTitleBar";
-import { Dropdown } from "react-bootstrap";
-import Iconify from "../../reusable/Iconify";
-import CustomTable from "../../reusable/CustomTable";
-import React, { useEffect, useState } from "react";
-import moment from "moment";
-import Helper from "../../../utils/Helper";
-import TopUpHistoryModel from "../../../models/TopUpHistoryModel";
-import { Space, Button as AntButton, } from "antd";
-import OrderModel from "models/OrderModel";
-import { Link } from "react-router-dom";
+import { Card, CardBody, Container, Row, Col, CardHeader } from 'reactstrap';
+import Palette from '../../../utils/Palette';
+import { Dropdown } from 'react-bootstrap';
+import Iconify from '../../reusable/Iconify';
+import CustomTable from '../../reusable/CustomTable';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import Helper from '../../../utils/Helper';
+import { Space, Button as AntButton } from 'antd';
+import OrderModel from 'models/OrderModel';
+import { Link } from 'react-router-dom';
 
 export default function OrderList() {
-    const [dataSource, setDataSource] = useState([]);
-    const [openTopUpModal, setOpenTopUpModal] = useState(false)
-    const [isNewRecord, setIsNewRecord] = useState(false)
-    const [loading, setLoading] = useState(false)
-    const [selectedTopUp, setSelectedTopUp] = useState(null)
+	const [barcoinUsages, setBarcoinUsages] = useState([]);
+	const [ridesUsages, setRidesUsages] = useState([]);
+	const [openTopUpModal, setOpenTopUpModal] = useState(false);
+	const [isNewRecord, setIsNewRecord] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [selectedTopUp, setSelectedTopUp] = useState(null);
 
-    const columns = [
-        {
-            id: 'created_at', label: 'Tanggal & jam', filter: true,
-            render: (row => {
-                return row?.created_at ? moment(row?.created_at).format('DD MMM YYYY, HH:mm') : '-'
-            })
-        },
-        {
-            id: 'user_id', label: 'User', filter: true,
-            render: (row => {
-                return row?.users?.username
-            })
-        },
-        {
-            id: 'quantity', label: 'Jumlah tiket dibeli', filter: true,
-            render: (row => {
-                let total = 0
-                for(let bd of row.barcoins_usage_detail){
-                    total+=bd.quantity
-                }
-                return total
-            })
-        },
-        {
-            id: 'total_coins', label: 'Koin Dipakai', filter: true,
-            render: (row => {
-                return <>
-                    <Iconify icon={'fluent-emoji-flat:coin'}></Iconify>
-                    {Helper.formatNumber(row.total_coins || 0)}
-                </>
-            })
-        },
-        {
-            id: 'action', label: '',
-            render: (row => {
-                return (
-                    <Dropdown
-                        menu={{
-                            items,
-                        }}
-                        trigger={['click']}
-                    >
-                        <a onClick={(e) => e.preventDefault()}>
-                            <Space>
-                                ...
-                            </Space>
-                        </a>
-                    </Dropdown>
-                )
-            })
-        }
-    ]
+	const barcoinUsagesTableColumns = [
+		{
+			id: 'created_at',
+			label: 'Tanggal & jam',
+			filter: true,
+			render: (row) => {
+				return row?.created_at ? moment(row?.created_at).format('DD MMM YYYY, HH:mm') : '-';
+			},
+		},
+		{
+			id: 'user_id',
+			label: 'User',
+			filter: true,
+			render: (row) => {
+				return row?.users?.username;
+			},
+		},
+		{
+			id: 'total_coins',
+			label: 'Koin Dipakai',
+			filter: true,
+			render: (row) => {
+				return (
+					<>
+						<Iconify icon={'fluent-emoji-flat:coin'}></Iconify>
+						{Helper.formatNumber(row.total_coins || 0)}
+					</>
+				);
+			},
+		},
+	];
 
-    const initializeData = async () => {
-        setLoading(true)
-        try {
-            console.log('masuk sinih')
-            let result = await OrderModel.getAll()
-            console.log('isi res', result)
-            setDataSource(result)
-            setLoading(false)
-        } catch (e) {
-            console.log('masuk sinih', e)
-            setLoading(false)
-        }
-    }
-
-    useEffect(() => {
-        initializeData()
-    }, [])
-
-    const items = [
+    const ridesUsagesTableColumns = [
         {
-            label: <a href="https://www.antgroup.com">1st menu item</a>,
-            key: '0',
-        },
-        {
-            label: <a href="https://www.aliyun.com">2nd menu item</a>,
-            key: '1',
-        },
-        {
-            type: 'divider',
-        },
-        {
-            label: '3rd menu item',
-            key: '3',
-        },
+			id: 'created_at',
+			label: 'Tanggal & jam',
+			filter: true,
+			render: (row) => {
+				return row?.created_at ? moment(row?.created_at).format('DD MMM YYYY, HH:mm') : '-';
+			},
+		},
+		{
+			id: 'user_id',
+			label: 'User',
+			filter: true,
+			render: (row) => {
+				return row?.users?.username;
+			},
+		},
+		{
+			id: 'total_rides',
+			label: 'Rides Digunakan',
+			filter: true,
+			render: (row) => {
+				return (
+					<>
+						{row.total_rides}
+					</>
+				);
+			},
+		}, 
     ];
 
-    return (
-        <>
-            <Container fluid>
-                <Row>
-                    <Col md={12} style={{ display: "flex", flexDirection: "column", alignItems: "flex-end" }}>
-                        <Link to="/orders/create">
-                            <AntButton style={{
-                                top: '10px', width: 200, marginBottom: 20
-                            }} onClick={() => {
+	const initializeData = async () => {
+		setLoading(true);
+		try {
+			let barcoinUsagesData = await OrderModel.getAllBarcoinUsages();
+			let ridesUsagesData = await OrderModel.getAllRidesUsages();
 
-                            }} size={'middle'} type={'primary'}>Tambah Order</AntButton>
-                        </Link>
+			setRidesUsages(ridesUsagesData);
+			setBarcoinUsages(barcoinUsagesData);
+		} catch (e) {
+			console.log(e);
+		}
+		setLoading(false);
+	};
 
-                        <Card style={{ background: Palette.BACKGROUND_DARK_GRAY, color: "white", width: "100%" }}
-                            className="card-stats mb-4 mb-xl-0">
+	useEffect(() => {
+		initializeData();
+	}, []);
 
-                            <CardBody>
-                                <CustomTable
-                                    showFilter={true}
-                                    pagination={true}
-                                    searchText={''}
-                                    data={dataSource}
-                                    columns={columns}
-                                />
-                            </CardBody>
-                        </Card>
-
-                    </Col>
-                </Row>
-
-            </Container>
-        </>
-    )
+	return (
+		<>
+			<Container fluid>
+				<Row>
+					<Col md={12} style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+						<Link to="/orders/create">
+							<AntButton
+								style={{
+									top: '10px',
+									width: 200,
+									marginBottom: 20,
+								}}
+								onClick={() => {}}
+								size={'middle'}
+								type={'primary'}
+							>
+								Tambah Order
+							</AntButton>
+						</Link>
+						<Card
+							style={{ background: Palette.BACKGROUND_DARK_GRAY, color: 'white', width: '100%' }}
+							className="card-stats mb-4 mb-xl-0"
+						>
+							<CardHeader style={{ background: Palette.BACKGROUND_DARK_GRAY, fontWeight: 700 }}>
+								Barcoins Order History
+							</CardHeader>
+							<CardBody>
+								<CustomTable
+									showFilter={true}
+									pagination={true}
+									searchText={''}
+									data={barcoinUsages}
+									columns={barcoinUsagesTableColumns}
+								/>
+							</CardBody>
+						</Card>
+						<Card
+							style={{ background: Palette.BACKGROUND_DARK_GRAY, color: 'white', width: '100%' }}
+							className="card-stats mb-4 mb-xl-0"
+						>
+							<CardHeader style={{ background: Palette.BACKGROUND_DARK_GRAY, fontWeight: 700 }}>
+								Rides Order History
+							</CardHeader>
+							<CardBody>
+								<CustomTable
+									showFilter={true}
+									pagination={true}
+									searchText={''}
+									data={ridesUsages}
+									columns={ridesUsagesTableColumns}
+								/>
+							</CardBody>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
 }
--- src/models/OrderModel.js
+++ src/models/OrderModel.js
@@ -1,15 +1,20 @@
 import ApiRequest from "../utils/ApiRequest";
 
 export default class OrderModel {
-    static getAll = async () => {
+    static getAllBarcoinUsages = async () => {
         return await ApiRequest.set("v1/barcoin/usage/all", "GET");
     }
+    
     static createBarcoinUsage = async (body) => {
         return await ApiRequest.set("v1/barcoin/usage/create", "POST", body);
     }
 
     static createBarcoinUsageV2 = async (body) => {
         return await ApiRequest.set('v2/barcoin/usage/create', 'POST', body);
+    }
+
+    static getAllRidesUsages = async () => {
+        return await ApiRequest.set('v1/rides', 'GET');
     }
 
     static createRidesUsage = async (body) => {

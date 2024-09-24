b8671084480d50b22e296f72ea4bfb781a6447bf
Michael Geraldi <work.michaelgeraldi@gmail.com>
2024-09-24 10:50:15 +0700

    update API endpoint

--- src/utils/ApiConfig.js
+++ src/utils/ApiConfig.js
@@ -1,3 +1,4 @@
 module.exports = {
-    base_url: "https://barcode-api.komunestudio.com/"
+    // base_url: "https://barcode-api.komunestudio.com/"
+    base_url: 'https://api.barcodegokart.com/'
 };

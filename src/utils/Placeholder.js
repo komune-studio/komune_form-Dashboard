0430b64397bfdda10dd4e02886844dfb6a65edac
TimothyAK <mothyabraham973@gmail.com>
2025-09-02 12:28:51 +0700

    update placeholder util

--- src/utils/Placeholder.js
+++ src/utils/Placeholder.js
@@ -1,16 +1,26 @@
 export default class Placeholder {
-  name = "e.g. John Doe"
-  phone = "e.g. +62 081233445566"
-  email = "e.g. example@email.com"
-  website = "www.example.com"
-  address = "e.g. Jl. Mawar No. 15, RT 005/RW 002, Kel. Mekar, Kec. Kencana, Kota Agung, 12345 or other shorter version"
-  biography = "insert biography here"
-  description = "insert description here"
-  language = "e.g. Indonesia, English, Spanish, French"
+  static name_person = "e.g. John Doe"
+  static name_book = "e.g. Laskar Pelangi"
+  static name_category = "e.g. Fiksi"
+  static name_publisher = "e.g. AAA Group / PT barabaca"
+  static phone = "e.g. +62 081233445566"
+  static email = "e.g. example@email.com"
+  static website = "www.example.com"
+  static address = "e.g. Jl. Mawar No. 15, RT 005/RW 002, Kel. Mekar, Kec. Kencana, Kota Agung, 12345 or other shorter version"
+  static biography = "Insert biography here"
+  static description = "Insert description here"
+  static language = "e.g. Indonesia, English, Spanish, French"
+  static facebook = "Facebook username"
+  static instagram = "Instagram username"
+  static tiktok = "TikTok username"
+  static twitter = "Twitter username"
+  static youtube = "YouTube channel name"
 
-  translated = {
-    name: "e.g. John Doe (Translated Version)",
-    biography: "insert biography here (Translated Version)",
-    description: "insert description here (Translated Version)",
+  static translated = {
+    name_person: "e.g. John Doe (Translated Version)",
+    name_book: "e.g. The Rainbow Troops (Translated Version)",
+    name_category: "e.g. Fiction (Translated Version)",
+    biography: "Insert biography here (Translated Version)",
+    description: "Insert description here (Translated Version)",
   }
 }

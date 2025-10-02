c7051edc084fa4b38bf01895f9ac0d9d726ba115
TimothyAK <mothyabraham973@gmail.com>
2025-10-02 14:22:38 +0700

    update cropper upload form component

--- src/components/reusable/CropperUploadForm.js
+++ src/components/reusable/CropperUploadForm.js
@@ -25,35 +25,37 @@ export default function CropperUploadForm({ label, name, imageAspect, onImageCha
       name={name}
       noStyle
     >
-      <Flex style={{ width: "100%", paddingBottom: "8px", justifyContent: "space-between", alignItems: "end" }}>
-        {label}
+      <Flex vertical style={{marginBottom: "24px"}}>
+        <Flex style={{ width: "100%", paddingBottom: "8px", justifyContent: "space-between", alignItems: "end" }}>
+          {label}
 
-        {/* <span className='label-link' onClick={handleEditCropper}>
+          {/* <span className='label-link' onClick={handleEditCropper}>
           Edit
         </span> */}
-      </Flex>
-      <CropperUpload
-        key={initialPreview}
-        uploadRef={uploadRef}
-        initialPreview={initialPreview}
-        onImageChange={onImageChange}
-        isOpen={isOpenCropper}
-        handleOpen={() => setIsOpenCropper(true)}
-        handleClose={() => setIsOpenCropper(false)}
-        imageAspect={imageAspect}
-      />
-      <Flex justify='start' style={{ marginTop: "4px", marginBottom: "24px" }}>
-        <Space wrap size={[8, 1]}>
-          <Typography.Text type="secondary" style={{ fontSize: "12px", display: "inline-block" }}>
-            Max image size 5MB
-          </Typography.Text>
-          <Typography.Text type="secondary" style={{ fontSize: "12px", display: "inline-block" }}>
-            -
-          </Typography.Text>
-          <Typography.Text type="secondary" style={{ fontSize: "12px", display: "inline-block" }}>
-            JPG, JPEG, PNG, WEBP supported
-          </Typography.Text>
-        </Space>
+        </Flex>
+        <CropperUpload
+          key={initialPreview}
+          uploadRef={uploadRef}
+          initialPreview={initialPreview}
+          onImageChange={onImageChange}
+          isOpen={isOpenCropper}
+          handleOpen={() => setIsOpenCropper(true)}
+          handleClose={() => setIsOpenCropper(false)}
+          imageAspect={imageAspect}
+        />
+        <Flex justify='start' style={{ marginTop: "4px" }}>
+          <Space wrap size={[8, 1]}>
+            <Typography.Text type="secondary" style={{ fontSize: "12px", display: "inline-block" }}>
+              Max image size 5MB
+            </Typography.Text>
+            <Typography.Text type="secondary" style={{ fontSize: "12px", display: "inline-block" }}>
+              -
+            </Typography.Text>
+            <Typography.Text type="secondary" style={{ fontSize: "12px", display: "inline-block" }}>
+              JPG, JPEG, PNG, WEBP supported
+            </Typography.Text>
+          </Space>
+        </Flex>
       </Flex>
     </Form.Item>
   )

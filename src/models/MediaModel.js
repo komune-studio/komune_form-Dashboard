8f5c0bae50ad375943d6a3039650d097b610767a
Kayden.C <kaydenciazaria@gmail.com>
2025-11-14 16:05:31 +0700

    update search and pagination using API in MediaList page

--- src/models/MediaModel.js
+++ src/models/MediaModel.js
@@ -9,6 +9,10 @@ export default class Media {
     return await ApiRequest.set(`v1/medias/getbyid/${id}`, "GET")
   }
 
+  static getAllWithPagination = async (limit, page, keyword) => {
+    return await ApiRequest.set(`v1/medias/get-with-pagination?page=${page}&limit=${limit}&sort_by=created_at&sort_type=asc&keyword=${keyword}`, "GET")
+  }
+
   static create = async (body) => {
     return await ApiRequest.set("v1/medias/create", "POST", body)
   }
